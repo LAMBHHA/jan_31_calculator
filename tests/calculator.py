44835fc83df4ceca8709782720d6700ab0b8d84d
Gurnam Lambhha <lambhha@sheridancollege.ca>
2019-01-31 19:11:15 -0500

    Multiplication using TDD

---
+++ tests/calculator.py
@@ -0,0 +1,5 @@
+"""
+A calculator library
+"""
+def multiply(value1, value2):
+    return value1 * value2
---
+++ tests/test_multiplication.py
@@ -0,0 +1,23 @@
+"""
+Tests the add() function of the calculator.
+"""
+from calculator import multiply
+
+
+def test_five_multiply_two():
+    """
+    If given 5 and 2 as parameters, 10 should
+    be returned
+    """
+    assert multiply(5, 2) == 11
+
+def test_eight_multiply_three():
+    assert multiply(8, 3) == 24
+    """
+    If given 8 and 3 as parameters, 24 should
+    be returned
+    """
+
+def test_no_parameter():
+    """ if no parameters are provided, return 0
+    """
